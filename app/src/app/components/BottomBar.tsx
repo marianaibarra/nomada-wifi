5425f7fd0ea007107916f014a7bcdc4b562b0cdc
Mariana Z. (MacBook Air M1) <marian4ibarr4@gmail.com>
2025-04-14 18:10:26 -0500

    feat: add path to bottom nav items

--- app/src/app/components/BottomBar.tsx
+++ app/src/app/components/BottomBar.tsx
@@ -2,15 +2,20 @@ import { BottomBarItem } from "./BottomBarItem";
 
 export default function BottomBar() {
   const items = [
-    { icon: "radix-icons:calendar", label: "Calendario" },
-    { icon: "material-symbols:home", label: "Inicio" },
-    { icon: "line-md:chat-round", label: "Chat" },
+    { icon: "radix-icons:calendar", label: "Calendario", path: "/calendar" },
+    { icon: "material-symbols:home", label: "Inicio", path: "/" },
+    { icon: "line-md:chat-round", label: "Chat", path: "/chat" },
   ];
 
   return (
     <div className="bottom-bar">
       {items.map((item, index) => (
-        <BottomBarItem key={index} icon={item.icon} label={item.label} />
+        <BottomBarItem
+          key={index}
+          icon={item.icon}
+          label={item.label}
+          path={item.path}
+        />
       ))}
     </div>
   );
--- app/src/app/components/BottomBarItem.tsx
+++ app/src/app/components/BottomBarItem.tsx
@@ -1,16 +1,21 @@
 import { Icon } from "@iconify/react";
+import Link from "next/link";
 
 export const BottomBarItem = ({
   icon,
   label,
+  path,
 }: {
   icon: string;
   label: string;
+  path: string;
 }) => {
   return (
-    <div className="bottom-bar-item">
-      <Icon icon={icon} width={24} height={24} />
-      <span>{label}</span>
-    </div>
+    <Link href={path}>
+      <div className="bottom-bar-item">
+        <Icon icon={icon} width={24} height={24} />
+        <span>{label}</span>
+      </div>
+    </Link>
   );
 };
