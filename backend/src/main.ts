6834c6bc172024404a1dcddd8ced9efb1ff267f8
Mariana Z. (MacBook Air M1) <marian4ibarr4@gmail.com>
2025-04-14 10:22:48 -0500

    feat: added configs to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { abortOnError: false });
+
+  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3001';
+
+  // Configs para development.
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+      hsts: false,
+    }),
+  );
+
+  // Config para usar class-validator
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: frontendUrl,
+    methods: ['GET', 'PATCH', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Nomada WiFi')
+    .setVersion('1.0')
+    .setDescription('API para Nomada WiFi')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
