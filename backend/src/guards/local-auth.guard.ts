ea775e8c593ea5d73f9c2a6ddfb478e0d8389401
Mariana Z. (MacBook Air M1) <marian4ibarr4@gmail.com>
2025-04-15 16:01:36 -0500

    feat: add validation errors login

--- backend/src/guards/local-auth.guard.ts
+++ backend/src/guards/local-auth.guard.ts
@@ -1,5 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { LoginUserDto } from 'src/users/dto/login-user.dto';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest<Request>();
+    const response = context.switchToHttp().getResponse<Response>();
+
+    const body = plainToClass(LoginUserDto, request.body);
+
+    const errors = await validate(body);
+
+    const errorMessages = errors.map((x: any) =>
+      Object.values(x.constraints).join(', '),
+    );
+
+    if (errorMessages.length > 0) {
+      throw new BadRequestException({
+        message: errorMessages,
+      });
+    }
+
+    return super.canActivate(context) as boolean | Promise<boolean>;
+  }
+}
